2b44a968c165bce597464c4db18accfbadc41b11
SirCobblestone <oteromirallesluis@gmail.com>
2026-08-17 16:55:26 +0200

    Migrate DSTracker example app from CocoaPods to Swift Package Manager

--- DSTracker-Example/AppDelegate.swift
+++ DSTracker-Example/AppDelegate.swift
@@ -1,26 +1,14 @@
-//
-//  AppDelegate.swift
-//  DS-SDK-Example
-//
-//  Created by DriveSmart IOS on 13/04/2020.
-//  Copyright © 2020 DriveSmart. All rights reserved.
-//
-
 import UIKit
 import DSTracker
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-
-        // MARK: - 2
         if let licenseKey = Bundle.main.infoDictionary?["DSTRACKER_LICENSE_KEY"] as? String {
-            Tracker.configure(licenseKey: licenseKey,
-                              doRequestPermissions: true) { outcome in
+            Tracker.configure(licenseKey: licenseKey, doRequestPermissions: true) { outcome in
                 if let error = outcome.failure {
                     print(error.localizedDescription)
                 } else if let success = outcome.success as? Bool, success {
@@ -30,15 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return true
     }
-    // MARK: UISceneSession Lifecycle
+
     func application(
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
 }
--- DSTracker-Example/PrintDebug.swift
+++ DSTracker-Example/PrintDebug.swift
@@ -1,17 +1,7 @@
-//
-//  PrintDebug.swift
-//  DriveSmart
-//
-//  Created by David Jardon on 8/6/17.
-//  Copyright © 2017 DriveSmart. All rights reserved.
-//
-//  Override print method to log only in debug mode
-//
-
 import Foundation
 
 func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
     #if DEBUG
-        Swift.print(items, separator:separator, terminator: terminator)
+    Swift.print(items, separator: separator, terminator: terminator)
     #endif
 }
